005e70aa9fb3db8c2c843d5c90d202fb30b760ae
Ameeya <crazy.ameeyadas@gmail.com>
2024-11-24 00:06:10 +0530

    updated the crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use chrono::{DateTime, TimeZone, Utc};
-use rustls::Session;
 use serde::{Deserialize, Serialize};
+use std::convert::TryInto;
 use std::fmt::Debug;
 use std::io::{Error, ErrorKind, Write};
+use std::iter::FromIterator;
 use std::net::TcpStream;
 use std::sync::Arc;
-use x509_parser::extensions::*;
 use x509_parser::objects::*;
-use x509_parser::parse_x509_der;
+use x509_parser::{extensions::*, parse_x509_certificate};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct ServerCert {
@@ -67,19 +67,17 @@ impl CheckSSL {
     ///   }
     /// }
     /// ```
-    pub fn from_domain_with_port(domain: &str, port: &str) -> Result<Cert, std::io::Error> {
-        let mut config = rustls::ClientConfig::new();
-        config
-            .root_store
-            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+    pub fn from_domain_with_port(domain: &'static str, port: &str) -> Result<Cert, std::io::Error> {
+        let root_store =
+            rustls::RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+        let config = rustls::ClientConfig::builder()
+            .with_root_certificates(root_store)
+            .with_no_client_auth();
 
         let rc_config = Arc::new(config);
-        let site = match webpki::DNSNameRef::try_from_ascii_str(domain) {
-            Ok(val) => val,
-            Err(e) => return Err(Error::new(ErrorKind::InvalidInput, e.to_string())),
-        };
+        let site = domain.try_into().unwrap();
 
-        let mut sess = rustls::ClientSession::new(&rc_config, site);
+        let mut sess = rustls::ClientConnection::new(rc_config, site).unwrap();
         let mut sock = TcpStream::connect(format!("{}:{}", domain, port))?;
         let mut tls = rustls::Stream::new(&mut sess, &mut sock);
 
@@ -119,27 +117,29 @@ impl CheckSSL {
             time_to_expiration: "".to_string(),
         };
 
-        if let Some(certificates) = tls.sess.get_peer_certificates() {
+        if let Some(certificates) = tls.conn.peer_certificates() {
             for certificate in certificates.iter() {
-                let x509cert = match parse_x509_der(certificate.as_ref()) {
+                let x509cert = match parse_x509_certificate(certificate.as_ref()) {
                     Ok((_, x509cert)) => x509cert,
                     Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string())),
                 };
 
-                let is_ca = match x509cert.tbs_certificate.basic_constraints() {
-                    Some((_, basic_constraints)) => basic_constraints.ca,
+                let is_ca = match x509cert.tbs_certificate.basic_constraints().unwrap() {
+                    Some(basic_extension) => basic_extension.value.ca,
                     None => false,
                 };
 
                 //check if it's ca or not, if ca then insert to intermediate certificate
                 if is_ca {
                     intermediate_cert.is_valid = x509cert.validity().is_valid();
-                    intermediate_cert.not_after =
-                        Utc.timestamp(x509cert.tbs_certificate.validity.not_after.timestamp(), 0);
-                    intermediate_cert.not_before =
-                        Utc.timestamp(x509cert.tbs_certificate.validity.not_before.timestamp(), 0);
+                    intermediate_cert.not_after = Utc
+                        .timestamp_opt(x509cert.tbs_certificate.validity.not_after.timestamp(), 0)
+                        .unwrap();
+                    intermediate_cert.not_before = Utc
+                        .timestamp_opt(x509cert.tbs_certificate.validity.not_before.timestamp(), 0)
+                        .unwrap();
 
-                    match oid2sn(&x509cert.signature_algorithm.algorithm) {
+                    match oid2sn(&x509cert.signature_algorithm.algorithm, oid_registry()) {
                         Ok(s) => {
                             intermediate_cert.signature_algorithm = s.to_string();
                         }
@@ -154,69 +154,68 @@ impl CheckSSL {
                     if let Some(time_to_expiration) =
                         x509cert.tbs_certificate.validity.time_to_expiration()
                     {
-                        intermediate_cert.time_to_expiration =
-                            format!("{:?} day(s)", time_to_expiration.as_secs() / 60 / 60 / 24)
+                        intermediate_cert.time_to_expiration = format!(
+                            "{:?} day(s)",
+                            time_to_expiration.whole_seconds() / 60 / 60 / 24
+                        )
                     }
 
                     let issuer = x509cert.issuer();
                     let subject = x509cert.subject();
 
-                    for rdn_seq in &issuer.rdn_seq {
-                        match oid2sn(&rdn_seq.set[0].attr_type) {
-                            Ok(s) => {
-                                let rdn_content = rdn_seq.set[0]
-                                    .attr_value
-                                    .content
-                                    .as_str()
-                                    .unwrap()
-                                    .to_string();
-                                if s == "CN" {
-                                    intermediate_cert.issuer = rdn_content;
+                    for rdn_seq in issuer.iter() {
+                        for attr_key_val in rdn_seq.iter() {
+                            match oid2sn(attr_key_val.attr_type(), oid_registry()) {
+                                Ok(s) => {
+                                    let rdn_content =
+                                        attr_key_val.attr_value().as_str().unwrap().to_string();
+                                    if s == "CN" {
+                                        intermediate_cert.issuer = rdn_content;
+                                    }
                                 }
-                            }
-                            Err(_e) => {
-                                return Err(Error::new(
-                                    ErrorKind::Other,
-                                    "Error converting Oid to Nid".to_string(),
-                                ))
+                                Err(_e) => {
+                                    return Err(Error::new(
+                                        ErrorKind::Other,
+                                        "Error converting Oid to Nid".to_string(),
+                                    ))
+                                }
                             }
                         }
                     }
-
-                    for rdn_seq in &subject.rdn_seq {
-                        match oid2sn(&rdn_seq.set[0].attr_type) {
-                            Ok(s) => {
-                                let rdn_content = rdn_seq.set[0]
-                                    .attr_value
-                                    .content
-                                    .as_str()
-                                    .unwrap()
-                                    .to_string();
-                                match s {
-                                    "C" => intermediate_cert.country = rdn_content,
-                                    "ST" => intermediate_cert.state = rdn_content,
-                                    "L" => intermediate_cert.locality = rdn_content,
-                                    "CN" => intermediate_cert.common_name = rdn_content,
-                                    "O" => intermediate_cert.organization = rdn_content,
-                                    _ => {}
+                    for rdn_seq in subject.iter() {
+                        for attr_key_val in rdn_seq.iter() {
+                            match oid2sn(attr_key_val.attr_type(), oid_registry()) {
+                                Ok(s) => {
+                                    let rdn_content =
+                                        attr_key_val.attr_value().as_str().unwrap().to_string();
+                                    match s {
+                                        "C" => intermediate_cert.country = rdn_content,
+                                        "ST" => intermediate_cert.state = rdn_content,
+                                        "L" => intermediate_cert.locality = rdn_content,
+                                        "CN" => intermediate_cert.common_name = rdn_content,
+                                        "O" => intermediate_cert.organization = rdn_content,
+                                        _ => {}
+                                    }
                                 }
-                            }
-                            Err(_e) => {
-                                return Err(Error::new(
-                                    ErrorKind::Other,
-                                    "Error converting Oid to Nid".to_string(),
-                                ))
+                                Err(_e) => {
+                                    return Err(Error::new(
+                                        ErrorKind::Other,
+                                        "Error converting Oid to Nid".to_string(),
+                                    ))
+                                }
                             }
                         }
                     }
                 } else {
                     server_cert.is_valid = x509cert.validity().is_valid();
-                    server_cert.not_after =
-                        Utc.timestamp(x509cert.tbs_certificate.validity.not_after.timestamp(), 0);
-                    server_cert.not_before =
-                        Utc.timestamp(x509cert.tbs_certificate.validity.not_before.timestamp(), 0);
+                    server_cert.not_after = Utc
+                        .timestamp_opt(x509cert.tbs_certificate.validity.not_after.timestamp(), 0)
+                        .unwrap();
+                    server_cert.not_before = Utc
+                        .timestamp_opt(x509cert.tbs_certificate.validity.not_before.timestamp(), 0)
+                        .unwrap();
 
-                    match oid2sn(&x509cert.signature_algorithm.algorithm) {
+                    match oid2sn(&x509cert.signature_algorithm.algorithm, oid_registry()) {
                         Ok(s) => {
                             server_cert.signature_algorithm = s.to_string();
                         }
@@ -228,8 +227,9 @@ impl CheckSSL {
                         }
                     }
 
-                    if let Some((_, san)) = x509cert.tbs_certificate.subject_alternative_name() {
-                        for name in san.general_names.iter() {
+                    if let Some(san) = x509cert.tbs_certificate.subject_alternative_name().unwrap()
+                    {
+                        for name in san.value.general_names.iter() {
                             match name {
                                 GeneralName::DNSName(dns) => server_cert.sans.push(dns.to_string()),
                                 _ => {}
@@ -240,58 +240,56 @@ impl CheckSSL {
                     if let Some(time_to_expiration) =
                         x509cert.tbs_certificate.validity.time_to_expiration()
                     {
-                        server_cert.time_to_expiration =
-                            format!("{:?} day(s)", time_to_expiration.as_secs() / 60 / 60 / 24)
+                        server_cert.time_to_expiration = format!(
+                            "{:?} day(s)",
+                            time_to_expiration.whole_seconds() / 60 / 60 / 24
+                        )
                     }
 
                     let issuer = x509cert.issuer();
                     let subject = x509cert.subject();
 
-                    for rdn_seq in &issuer.rdn_seq {
-                        match oid2sn(&rdn_seq.set[0].attr_type) {
-                            Ok(s) => {
-                                let rdn_content = rdn_seq.set[0]
-                                    .attr_value
-                                    .content
-                                    .as_str()
-                                    .unwrap()
-                                    .to_string();
-                                if s == "CN" {
-                                    server_cert.issuer = rdn_content;
+                    for rdn_seq in issuer.iter() {
+                        for attr_key_val in rdn_seq.iter() {
+                            match oid2sn(attr_key_val.attr_type(), oid_registry()) {
+                                Ok(s) => {
+                                    let rdn_content =
+                                        attr_key_val.attr_value().as_str().unwrap().to_string();
+                                    if s == "CN" {
+                                        server_cert.issuer = rdn_content;
+                                    }
                                 }
-                            }
-                            Err(_e) => {
-                                return Err(Error::new(
-                                    ErrorKind::Other,
-                                    "Error converting Oid to Nid".to_string(),
-                                ))
+                                Err(_e) => {
+                                    return Err(Error::new(
+                                        ErrorKind::Other,
+                                        "Error converting Oid to Nid".to_string(),
+                                    ))
+                                }
                             }
                         }
                     }
 
-                    for rdn_seq in &subject.rdn_seq {
-                        match oid2sn(&rdn_seq.set[0].attr_type) {
-                            Ok(s) => {
-                                let rdn_content = rdn_seq.set[0]
-                                    .attr_value
-                                    .content
-                                    .as_str()
-                                    .unwrap()
-                                    .to_string();
-                                match s {
-                                    "C" => server_cert.country = rdn_content,
-                                    "ST" => server_cert.state = rdn_content,
-                                    "L" => server_cert.locality = rdn_content,
-                                    "CN" => server_cert.common_name = rdn_content,
-                                    "O" => server_cert.organization = rdn_content,
-                                    _ => {}
+                    for rdn_seq in subject.iter() {
+                        for attr_key_val in rdn_seq.iter() {
+                            match oid2sn(attr_key_val.attr_type(), oid_registry()) {
+                                Ok(s) => {
+                                    let rdn_content =
+                                        attr_key_val.attr_value().as_str().unwrap().to_string();
+                                    match s {
+                                        "C" => server_cert.country = rdn_content,
+                                        "ST" => server_cert.state = rdn_content,
+                                        "L" => server_cert.locality = rdn_content,
+                                        "CN" => server_cert.common_name = rdn_content,
+                                        "O" => server_cert.organization = rdn_content,
+                                        _ => {}
+                                    }
                                 }
-                            }
-                            Err(_e) => {
-                                return Err(Error::new(
-                                    ErrorKind::Other,
-                                    "Error converting Oid to Nid".to_string(),
-                                ))
+                                Err(_e) => {
+                                    return Err(Error::new(
+                                        ErrorKind::Other,
+                                        "Error converting Oid to Nid".to_string(),
+                                    ))
+                                }
                             }
                         }
                     }
